45d257387583d96e44ea37a858dc763b0eaf76a8
lfaure75 <febus95@hotmail.com>
2024-10-16 13:51:49 +0200

    configure remote config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {federation} from "@module-federation/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'reactNewViteRemote',
+      filename: 'remoteReactNewViteEntry.js',
+      // Modules to expose
+      exposes: {
+        './Module': './src/App.jsx',
+      },
+      shared: ['react', 'react-dom']
+    }),
+  ],
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
 })
